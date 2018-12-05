7634aa7789544ec399c282068efb5e42ad9395ab
Dmitrii <kislenko1910@mail.ru>
2018-12-05 14:11:04 +0200

    Added convertion table

--- menu 3/main.js
+++ menu 3/main.js
@@ -175,3 +175,27 @@ else
 
 }
 }
+function table(t,i)
+{
+    var k;
+    k=(t.toString(i));
+    k = k.toString().toUpperCase();
+    
+    return (k);
+}
+function turnOn()
+{
+ if (document.getElementById('checkButton').textContent =='Show') 
+ {
+     document.getElementById('table').style.visibility ='visible';
+     document.getElementById('checkButton').textContent ='Clean';
+   
+}
+else
+ {
+     document.getElementById('table').style.visibility ='hidden';
+     document.getElementById('checkButton').textContent ='Show';
+   
+}
+
+}
